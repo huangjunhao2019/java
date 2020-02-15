aeea4c3a009eb8ff149625b7c3863705cf0af2f3
huangjunhao <huang.jun.hao@foxmail.com>
2020-02-15 17:51:23 +0800

    vector, stack 都能直接输出”

---
+++ 2.15/VectorTest.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class VectorTest{
+	public static void main(String[] args){
+		Vector<Integer> a=new Vector<Integer>();
+		for(int i=0;i<5;i++)
+			a.add(i);
+		System.out.println(a);
+	}
+}
